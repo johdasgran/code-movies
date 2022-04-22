049e9e56c5081e98f6981503982324deceada5ba
Johdasgran <dampig14@gmail.com>
2022-04-21 19:59:53 -0500

    update

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 
-let container = document.querySelector("#container");
+// let container = document.querySelector("#container");
 
-for (let i = 0; i < 25; i++) {
-    let botton = document.createElement("div");
-    botton.className = "botton";
-    container.appendChild(botton);
-    botton.addEventListener("click", ()=>{
-        botton.className = "botton press";
-    })
-}
+// for (let i = 0; i < 25; i++) {
+//     let botton = document.createElement("div");
+//     botton.className = "botton";
+//     container.appendChild(botton);
+//     botton.addEventListener("click", ()=>{
+//         botton.className = "botton press";
+//     })
+// }
 
 /*** */
 
 const square = document.getElementById("dropzone");
 
 
-square.addEventListener("drop", ()=> {
-    console-log("sdasdas")
-});
-
 
 square.ondrop(() => {
     console.log("asdas")
 })
+
+
+
+
+
+
+
+
+
+
+
+// square.addEventListener("drop", (event)=> {
+//     event.preventDefault();
+//     console-log("sdasdas")
+// });
+
+
